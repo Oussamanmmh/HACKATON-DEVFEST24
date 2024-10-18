31cc7f30391f2820646cd31c4262e4950af37112
Younes Hellalet <127552249+Kind-Unes@users.noreply.github.com>
2024-10-18 11:10:56 +0100

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,55 +1,53 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  id: { type: Number, unique: true }, 
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String }, 
-  googleId: { type: String, default: null }, 
-  appleId: { type: String, default: null }, 
-  authProvider: { type: String, enum: ['google', 'apple', 'email'], default: 'email' }, 
-  role: { type: String, enum: ['user', 'delivery', 'admin'], default: 'user' },
-  refreshToken: { type: String },
-  isVerified: { type: Boolean, default: false },
-  resetPasswordToken: { type: String },
-  resetPasswordExpires: { type: Date },
-  phoneNumber: { type: String,   },  
-  address: { type: String,   }, 
-  savedAddresses: [{ type: String, default: [] }], 
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', default: [] }],
-  orderHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order', default: [] }],
-  profileImage: {
-    thumbnail: { type: String,   }, 
-    full: { type: String,   }, 
-    default: { type: String,   }, 
+const userSchema = new mongoose.Schema(
+  {
+    id: { type: Number, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    googleId: { type: String, default: null }, // for scalablity
+    appleId: { type: String, default: null }, // for scalablity
+    authProvider: {
+      type: String,
+      enum: ["email", "apple", "google"],
+      default: "email",
+    }, // for scalablity
+    role: { type: String, enum: ["worker", "manager"], default: "worker" },
+    refreshToken: { type: String },
+    isVerified: { type: Boolean, default: false }, // for scalablity
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
+    phoneNumber: { type: String }, // for scalablity
+    profileImage: {
+      thumbnail: { type: String },
+      full: { type: String },
+      default: { type: String },
+    },
+    // add otp
+    otp: { type: String }, // for scalablity
+    otpExpires: { type: Date }, // for scalablity
+    notificationsToken: { type: String },
+    status: { type: String, enum: ["active", "inactive"], default: "active" }, // for scalablity
+    bio: { type: String }, // for scalablity
+    language: { type: String, default: "en" }, // for scalablity
+    lastLogin: { type: Date }, // for scalablity
+
+    socialMediaLinks: {
+      // for scalablity
+      facebook: { type: String },
+      twitter: { type: String },
+      instagram: { type: String },
+    },
   },
-  // add otp
-  otp: { type: String,   },
-  otpExpires: { type: Date },
-  notificationsToken: { type: String,   },
-  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
-  bio: { type: String,   },  
-  language: { type: String, default: 'en' },
-  lastLogin: { type: Date },
-  reviewCount: { type: Number, default: 0 },
-  locationCoordinates: {
-    lat: { type: Number, default: 0 },  
-    lon: { type: Number, default: 0 },  
-  },
-  socialMediaLinks: {
-    facebook: { type: String,   },
-    twitter: { type: String,   },
-    instagram: { type: String,   },
-  },  
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-
-
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
   }
@@ -60,6 +58,5 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
